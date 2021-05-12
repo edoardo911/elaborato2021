568eacceef51e78bb965857a8b084ab00beadd9f
edoardo911 <60601060+edoardo911@users.noreply.github.com>
2021-05-12 12:10:05 +0200

    Add files via upload

--- src/main/java/it/main/MainActivity.java
+++ src/main/java/it/main/MainActivity.java
@@ -2,14 +2,55 @@ package it.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 public class MainActivity extends AppCompatActivity
 {
+    PendingIntent intent;
+    NfcAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        adapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if(adapter != null && adapter.isEnabled())
+            adapter.enableForegroundDispatch(this, intent, null, null);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if(adapter != null) adapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        setIntent(intent);
+
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction()))
+        {
+            System.out.println(intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES));
+        }
     }
 }
--- src/main/java/it/main/NFCFragment.java
+++ src/main/java/it/main/NFCFragment.java
@@ -1,10 +1,17 @@
 package it.main;
 
+import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,27 +21,15 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import java.io.UnsupportedEncodingException;
+
 public class NFCFragment extends Fragment
 {
-    NfcAdapter adapter;
-    PendingIntent intent;
-    IntentFilter filter;
-
     public NFCFragment() { super(R.layout.nfc_fragment); }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
-        adapter = NfcAdapter.getDefaultAdapter(getContext());
-        if(adapter == null)
-        {
-            Toast.makeText(getContext(), "NFC not supported", Toast.LENGTH_SHORT);
-            NavController nav = Navigation.findNavController(view);
-            nav.navigate(R.id.action_nfc_fragment_to_registerFragment);
-        }
-
-        //readFromIntent(getActivity().getIntent());
-        intent = PendingIntent.getActivity(getContext(), 0, new Intent(getContext(), getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
     }
 }
